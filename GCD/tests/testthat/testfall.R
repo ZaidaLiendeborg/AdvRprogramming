6c7cce7b65f03924a147fbdff9f8f955fdd84929
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-14 16:41:23 +0200

    Fourth commit

--- GCD/tests/testthat/testfall.R
+++ GCD/tests/testthat/testfall.R
@@ -1,4 +1,6 @@
 test_that("euclidean",{
   expect_that(euclidean, is_a("function"),
               info = "False: euclidean is not a function.")
+  expect_that(is.numeric(euclidean(a=100, b=10000)), condition=is_true(),
+              info = "False: Euclidean does not return numeric output.")
 })
