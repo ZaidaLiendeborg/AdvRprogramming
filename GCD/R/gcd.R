0e39ccc96c44521a7d90b029a10a7581b323644a
ZaidaLiendeborg <zaida@liendeborg.se>
2016-09-14 13:43:00 +0200

    Third commit

---
+++ GCD/R/gcd.R
@@ -0,0 +1,10 @@
+#' GCD: A package for computing the GCD of two numbers
+#'
+#' The GCD package contains one function, the euclidean function. 
+#' 
+#' @section euclidean functions:
+#' The \link[GCD]{euclidean} function takes GCD of two numbers using euclidean algortihm.
+#'
+#' @docType package
+#' @name GCD
+NULL
